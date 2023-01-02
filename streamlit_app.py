e578b95a6c62a35a62293bb9423c12c2d5df9c02
Swapnil Vyom <121800261+vyomtrysnowflake@users.noreply.github.com>
2023-01-02 17:53:09 +0530

    Update streamlit_app.py
    adding select widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
 streamlit.dataframe(my_fruit_list)

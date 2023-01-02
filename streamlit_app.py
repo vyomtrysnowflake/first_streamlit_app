fa6ebd3f0196a69a3070af2ed4455da7614d13dd
Swapnil Vyom <121800261+vyomtrysnowflake@users.noreply.github.com>
2023-01-02 17:51:43 +0530

    Update streamlit_app.py
    adding df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
-
+import pandas
 streamlit.title("my first app, this is easy")
 
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

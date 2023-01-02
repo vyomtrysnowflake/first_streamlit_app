1abc2ce3fbd25215e8c968071dd54aca0e49f603
Swapnil Vyom <121800261+vyomtrysnowflake@users.noreply.github.com>
2023-01-02 17:46:28 +0530

    Update streamlit_app.py
    add comm

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("my first app, this is easy")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

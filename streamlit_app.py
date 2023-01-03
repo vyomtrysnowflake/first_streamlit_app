8855f898955aeb1eb282c12f0193746bb065b2d1
Swapnil Vyom <121800261+vyomtrysnowflake@users.noreply.github.com>
2023-01-03 19:41:57 +0530

    Update streamlit_app.py
    change header

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)

d355fe466ae7545b15e4954e1423fd18cf12f5c7
Swapnil Vyom <121800261+vyomtrysnowflake@users.noreply.github.com>
2023-01-05 13:11:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_add = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', fruit_add)
